77742e15de212f1416f345e0823cfc4ae1de5086
Krzysztof Kucharzyk <krzysztofkucharzyk@gmail.com>
2022-07-17 12:23:42 +0200

    poprawione ostateczne

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 let przycisk = document.querySelector('button');
 
-let inputImie = document.getElementById("imie").value;
-let inputNazwisko = document.getElementById('nazwisko').value;
-let inputTelefon = document.getElementById('telefon').value;
-
-let pName = document.getElementById('name');
-let pSurname = document.getElementById('surname');
-let pPhone = document.getElementById('phone');
-
 przycisk.onclick = function () {
-    pName.innerText = inputImie;
-    pSurname.innerText = inputNazwisko;
-    pPhone.innerText = inputTelefon;
+    let inputImie = document.getElementById("imie").value;
+    let inputNazwisko = document.getElementById('nazwisko').value;
+    let inputTelefon = document.getElementById('telefon').value;
+
+    let pName = document.getElementById('name');
+    let pSurname = document.getElementById('surname');
+    let pPhone = document.getElementById('phone');
+    
+    pName.innerHTML = 'Twoje imię: ' + inputImie;
+    pSurname.innerHTML = 'Twoje nazwisko: ' + inputNazwisko;
+    pPhone.innerHTML = 'Twój telefon: ' + inputTelefon;
 }
